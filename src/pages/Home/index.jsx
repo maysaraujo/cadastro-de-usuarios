54c7b944caa71a050f9723dd9884756c0ca19b38
maysaraujo <maysaaraujo25@gmail.com>
2024-06-17 13:12:49 -0300

    feat: updating the Home and its styles

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,9 +1,50 @@
 import './style.css';
+import Trash from '../../assets/trash.svg';
 
 function Home() {
+  const users = [
+    {
+      id: '234asdasd',
+      name: 'Maysa',
+      age: 20,
+      email: 'maysa@email.com',
+    },
+    {
+      id: '56765asdfsd',
+      name: 'João',
+      age: 22,
+      email: 'joao@email.com',
+    },
+  ];
+
   return (
-    <div>
-      <h1>Olá React</h1>
+    <div className='container'>
+      <form action='POST'>
+        <h1>Cadastro de Usuários</h1>
+        <input name='name' type='text' placeholder='Nome' />
+        <input name='age' type='number' placeholder='Idade' />
+        <input name='email' type='email' placeholder='Email' />
+        <button type='button'>Cadastrar</button>
+      </form>
+
+      {users.map((user) => (
+        <div key={user.id} className='card'>
+          <div>
+            <p>
+              Nome: <span>{user.name}</span>{' '}
+            </p>
+            <p>
+              Idade: <span>{user.age}</span>{' '}
+            </p>
+            <p>
+              Email: <span>{user.email}</span>{' '}
+            </p>
+          </div>
+          <button>
+            <img src={Trash} alt='Trash icon' />
+          </button>
+        </div>
+      ))}
     </div>
   );
 }
